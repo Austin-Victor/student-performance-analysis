874c069a9f4e7080761952538989ef208279ee97
Austin Victor <austinev698@gmail.com>
2025-08-11 01:15:13 +0100

    Update

--- student_performance_analysis.r
+++ student_performance_analysis.r
@@ -35,37 +35,55 @@ unique(student_data$Tutoring_Sessions)
 unique(student_data$Family_Income)
 unique(student_data$Teacher_Quality)
 #drops all rows with the Teacher_Quality column empty and assign to a new frame
-clean1_data <- student_data[student_data$Teacher_Quality != "", ]
-unique(clean1_data$Teacher_Quality)
+student_data <- student_data[student_data$Teacher_Quality != "", ]
+unique(student_data$Teacher_Quality)
 
-unique(clean1_data$School_Type)
-unique(clean1_data$Peer_Influence)
-unique(clean1_data$Physical_Activity)
-unique(clean1_data$Learning_Disabilities)
+unique(student_data$School_Type)
+unique(student_data$Peer_Influence)
+unique(student_data$Physical_Activity)
+unique(student_data$Learning_Disabilities)
 
-unique(clean1_data$Parental_Education_Level)
+unique(student_data$Parental_Education_Level)
 #drops all rows with the Parental_Education_Level column empty
-clean1_data <- clean1_data[clean1_data$Parental_Education_Level != "", ]
+student_data <- student_data[student_data$Parental_Education_Level != "", ]
 
-unique(clean1_data$Distance_from_Home)
+unique(student_data$Distance_from_Home)
 #drops all rows with the Distance_from_Home column empty
-clean1_data <- clean1_data[clean1_data$Distance_from_Home != "", ]
+student_data <- student_data[student_data$Distance_from_Home != "", ]
 
-unique(clean1_data$Gender)
-unique(clean1_data$Exam_Score)
+unique(student_data$Gender)
+unique(student_data$Exam_Score)
 
-dim(clean1_data)
-colSums(is.na(clean1_data))
-str(clean1_data)
+dim(student_data)
+colSums(is.na(student_data))
+str(student_data)
 
 #EDA(EXPLORATORY DATA ANALYSIS)
-summary(clean1_data)
-table(clean1_data$Access_to_Resources)
+summary(student_data)
+table(student_data$Access_to_Resources)
 
+#RELATIONSHIPS
+#Create a composite metric called level of dedication to school work
+#create a composite metric called level of exam performance
+#Family_Income vs Level of Exam performance
+#Gender vs level of dedication
+#Level of Dedication vs Exam Score
+#Parental Involvement vs Gender
+#Gender vs Study time
+
+student_data <- student_data %>%
+  mutate(
+    norm_hours = (
+      (Hours_Studied) - min(Hours_Studied))/(max(Hours_Studied) - min(Hours_Studied)
+    )
+  )
+
+student_data <- student_data %>%
+  mutate(norm_attendance = (Attendance / 100))
+
+student_data$Motivation_Level <- as.num
 
 
 #RELATIONSHIP BETWEEEN FAMILY INCOME AND EXAMSCORE
-low_income_grades <- clean1_data %>%
-  filter(Family_Income == "low") %>%
-  select(Exam_Score)
-View(clean1_data)
+
+View(student_data)
